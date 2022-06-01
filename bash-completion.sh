6a3238dc376d63c5a38227aae4e992f50208f0b1
Gernot Schulz <gernot@intevation.de>
2022-06-01 11:55:42 +0200

    Update bash completion

--- bash-completion.sh
+++ bash-completion.sh
@@ -5,7 +5,7 @@ _os4instancectl()
   cur="${COMP_WORDS[COMP_CWORD]}"
   prev="${COMP_WORDS[COMP_CWORD-1]}"
   opts="help ls add rm start stop update erase lock unlock autoscale manage"
-  opts+=" --help --force --color --project-dir --fast --patient --no-pid-file --no-add-account"
+  opts+=" --help --force --color --project-dir --fast --patient --no-pid-file --no-add-account --verbose"
   opts+=" --compose-template --config-template"
   opts+=" --long --metadata --online --offline --error --version --locked --unlocked"
   opts+=" --clone-from --local-only"
@@ -13,7 +13,7 @@ _os4instancectl()
   opts+=" --migrations-finalize --migrations-no-ask"
   opts+=" --accounts --dry-run"
   opts+=" --action"
-  diropts="ls|rm|start|stop|update|erase|lock|unlock|autoscale|manage|--clone-from|--"
+  diropts="ls|rm|start|stop|update|erase|lock|unlock|autoscale|manage|--clone-from"
 
   if [[ ${prev} =~ ${diropts} ]]; then
     COMPREPLY=( $(cd /srv/openslides/os4-instances && compgen -d -- ${cur}) )
